2c801cc97b1544507664312b87c627d86abcc236
mariembenhassen <121687274+mariembenhassen@users.noreply.github.com>
2024-07-24 00:47:17 +0100

    update ui

--- lib/screens/Messagerie_screen/Teacher_messages/Abscence_Demande/Abscence.dart
+++ lib/screens/Messagerie_screen/Teacher_messages/Abscence_Demande/Abscence.dart
@@ -52,14 +52,14 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
               content: Text('Demande d\'absence envoyée avec succès !'),
-              backgroundColor: Colors.green,
+              backgroundColor: Colors.blue[800],
             ),
           );
         } else {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
-              content:
-                  Text('Échec de l\'envoi de la demande : ${responseData['message']}'),
+              content: Text(
+                  'Échec de l\'envoi de la demande : ${responseData['message']}'),
               backgroundColor: Colors.red,
             ),
           );
@@ -67,7 +67,8 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text('La demande a échoué avec le statut: ${response.statusCode}'),
+            content: Text(
+                'La demande a échoué avec le statut: ${response.statusCode}'),
             backgroundColor: Colors.red,
           ),
         );
@@ -75,10 +76,53 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text(' Une erreur s\'est produite: $e'),
+          content: Text('Une erreur s\'est produite: $e'),
           backgroundColor: Colors.red,
         ),
       );
+    }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.light().copyWith(
+            primaryColor:
+                Colors.blue[800], // Blue color for the header and buttons
+            // Styling for calendar and date selection
+            dialogBackgroundColor: Colors.white,
+            textTheme: TextTheme(
+              headline6:
+                  TextStyle(color: Colors.black), // Black color for date text
+              bodyText2:
+                  TextStyle(color: Colors.black), // Black color for body text
+            ),
+            buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blue[800], // Blue color for buttons
+            ),
+            // Styling for input fields
+            inputDecorationTheme: InputDecorationTheme(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              hintStyle: TextStyle(color: Colors.grey[600]),
+            ),
+          ),
+          child: child!,
+        );
+      },
+    );
+    if (picked != null && picked != DateTime.now()) {
+      setState(() {
+        _dateController.text = picked.toLocal().toString().split(' ')[0];
+      });
     }
   }
 
@@ -88,7 +132,7 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
       appBar: AppBar(
         title: Text(
           'Demande d\'Absence',
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.white, fontSize: 20),
         ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
@@ -96,42 +140,48 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
         ),
         backgroundColor: Colors.blue[800],
       ),
-      backgroundColor: Colors.grey[100], // Light grey background
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'J\'espère que vous allez bien!',
-              style: Theme.of(context).textTheme.headline5?.copyWith(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                    color: Colors.blueGrey[800],
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Bienvenue',
+                    style: Theme.of(context).textTheme.headline4?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
                   ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Veuillez remplir les détails ci-dessous pour soumettre votre demande d\'absence.',
+                    style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                          color: Colors.black,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
             SizedBox(height: 20),
-            Text(
-              'Merci de compléter les informations suivantes pour soumettre votre demande d\'absence.',
-              style: Theme.of(context).textTheme.headline5?.copyWith(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                    color: Colors.blueGrey[800],
-                  ),
-            ),
+            Divider(color: Colors.blue[800]),
             SizedBox(height: 20),
             Text(
               'Observation:',
               style: Theme.of(context).textTheme.subtitle1?.copyWith(
                     fontWeight: FontWeight.bold,
-                    color: Colors.blueGrey[800],
+                    color: Colors.black,
                   ),
             ),
             SizedBox(height: 8),
             TextField(
               controller: _observationController,
               maxLines: 4,
-              style: TextStyle(color: Colors.black), // Set text color to black
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
@@ -139,8 +189,7 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
                 filled: true,
                 fillColor: Colors.white,
                 hintText: 'Entrez votre observation ici',
-                hintStyle:
-                    TextStyle(color: Colors.grey[600]), // Light grey hint text
+                hintStyle: TextStyle(color: Colors.grey[600]),
               ),
             ),
             SizedBox(height: 20),
@@ -148,21 +197,26 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
               'Date:',
               style: Theme.of(context).textTheme.subtitle1?.copyWith(
                     fontWeight: FontWeight.bold,
-                    color: Colors.blueGrey[800],
+                    color: Colors.black,
                   ),
             ),
             SizedBox(height: 8),
-            TextField(
+            TextFormField(
               controller: _dateController,
-              style: TextStyle(color: Colors.black), // Set text color to black
+              readOnly: true,
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
                 filled: true,
                 fillColor: Colors.white,
-                hintText: 'Entrez la date (YYYY-MM-DD)',
+                hintText: 'YYYY-MM-DD',
                 hintStyle: TextStyle(color: Colors.grey[600]),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.calendar_today, color: Colors.blue[800]),
+                  onPressed: () => _selectDate(context),
+                ),
               ),
             ),
             SizedBox(height: 20),
@@ -174,11 +228,22 @@ class _AbscenceScreenState extends State<AbscenceScreen> {
                   style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue[800], // Button color
+                  backgroundColor: Colors.blue[800],
                   padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12),
                   ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Center(
+              child: Text(
+                'Merci de votre coopération!',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
                 ),
               ),
             ),
